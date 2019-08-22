e72409c08aca0c4fe47189e719a72f2c682dae13
foresitegroup <mark@foresitegrp.com>
2019-08-22 10:25:38 -0500

    Change bcc address

--- form-contact.php
+++ form-contact.php
@@ -15,7 +15,7 @@ if ($_POST['confirmationCAP'] == "") {
     $SendTo = "frank.butterfield@kommerlingusa.com";
     $Headers = "From: Razor Contact Form <contactform@razortrimproducts.com>\r\n";
     $Headers .= "Reply-To: " . $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\r\n";
-    $Headers .= "Bcc: mark@foresitegrp.com\r\n";
+    $Headers .= "Bcc: foresitegroupllc@gmail.com\r\n";
 
     $Message = $_POST[md5('name' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\n";
     $Message .= $_POST[md5('email' . $_POST['ip'] . $salt . $_POST['timestamp'])] . "\n";
