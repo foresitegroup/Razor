1b62cc0c233423c92da40d467cbaa5aafad162e9
foresitegroup <mark@foresitegrp.com>
2022-06-08 09:33:08 -0500

    Add new analytics code

--- index.php
+++ index.php
@@ -23,6 +23,15 @@
         $("a[href$='.pdf']").prop('target', 'new');
       });
     </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4H9PNX77ZW"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4H9PNX77ZW');
+    </script>
   </head>
   <body>
 
